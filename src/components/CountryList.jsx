a1f63929d51ad2b0d708c853395eb35bc37dcef4
Chinmay-G <chinmay.the1@gmail.com>
2025-06-05 10:30:18 +0530

    Routes and navigations. DoNext: Implement Context API

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -21,7 +21,7 @@ function CountryList({ cities, isLoading }) {
     <div>
       <ul className={styles.countryList}>
         {countries.map((country) => (
-          <CountryItem country={country} />
+          <CountryItem country={country} key={country.country} />
         ))}
       </ul>
     </div>
